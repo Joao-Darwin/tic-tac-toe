72a2a1dda1ff80a21c152444a984d79f7e1d44d3
João Darwin <joaodarwin.ip22@gmail.com>
2024-05-22 16:26:57 -0300

    feat: Aumentar font do texto com o nome do jogador

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     playerText: {
-        fontSize: 18,
+        fontSize: 20,
         fontWeight: '500',
     },
     scoreText: {
